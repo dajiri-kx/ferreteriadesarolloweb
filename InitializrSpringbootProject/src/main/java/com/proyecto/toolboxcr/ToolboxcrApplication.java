d48ad04b7b6d6e790d3df94144289d3165c32d07
Daniel delas Cinco Ordilias <daniconix98@gmail.com>
2026-07-08 22:47:36 -0600

    Semana 8.5: Creación de Proyecto

---
+++ InitializrSpringbootProject/src/main/java/com/proyecto/toolboxcr/ToolboxcrApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.toolboxcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolboxcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolboxcrApplication.class, args);
+	}
+
+}
